7dba8f56ddeb69ca73490005a719c9c44680a23e
Dan Strokirk <dan@kundo.se>
2020-08-18 11:10:38 +0200

    :sunrise: Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import './scss/global.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FunctionalComponent = () => <h1>hello-world</h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
