ca08a53ba5d26484ab705977eb19e91e4c41cb81
MattiasOlla <mattias.olla@gmail.com>
2020-08-18 15:07:53 +0200

    Basic parsing of json choreography for algorithm.

---
+++ server/dancers.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3.8
+
+from __future__ import annotations
+
+import json
+from typing import Any, Callable, Dict, Generator, Tuple, List
+
+with open("test_data.json") as f:
+    test_data = json.load(f)
+
+
+class Dance:
+    def __init__(self, json_object: Dict[str, Any]) -> None:
+        self.json = json_object
+        self.title: str = json_object.get("title", None)
+        self.id: int = json_object.get("id", None)
+        self.choreography: List[Dict[str, Any]] = json_object.get("choreography", [])
+
+    def _vectors(self) -> Generator[Tuple[int, ...]]:
+        for frame in self.choreography:
+            vector = []
+            for pos in sorted(frame["positions"], key=lambda pos: pos["name"]):
+                vector.extend([pos["x"], pos["y"]])
+            yield tuple(vector)
+
+    def vector_pairs(self) -> Generator[Tuple[Tuple[int, ...], Tuple[int, ...]]]:
+        prev = None
+        for vec in self._vectors():
+            if prev is not None:
+                yield prev, vec
+            prev = vec
+
+
+if __name__ == "__main__":
+    dance = Dance(test_data)
+    for x in dance.vector_pairs():
+        print(x)
