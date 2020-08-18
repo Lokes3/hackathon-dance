d64230d44e62735cb1658c0e6971058bc4c81fd3
MattiasOlla <mattias.olla@gmail.com>
2020-08-18 18:36:44 +0200

    Interface update Json data can now be parsed, inerpolated, and returned as json

--- server/dancers.py
+++ server/dancers.py
@@ -5,7 +5,7 @@ from typing import Any, Callable, Dict, Generator, NamedTuple, Tuple, List
 
 from icecream import ic
 
-with open("test_data.json", encoding="latin-1") as f:
+with open("test_data.json", encoding="utf-8") as f:
     test_data = json.load(f)
 
 
@@ -23,8 +23,8 @@ Algorithm = Callable[[List[Vector], List[Vector], int, int], List[List[Vector]]]
 class Dance:
     def __init__(self, json_object: Dict[str, Any]) -> None:
         self.title: str = json_object["title"]
-        dim = json_object["dimensions"]
-        self.dimensions: List[int] = [2 * dim[d] + 1 for d in ["columns", "rows"]]
+        self.dimensions: List[int] = json_object["dimensions"]
+        self.real_dimensions = [2 * self.dimensions[d] + 1 for d in ["columns", "rows"]]
         self.id: int = json_object["id"]
         self.choreography: List[Dict[str, Any]] = json_object["choreography"]
         for frame in self.choreography:
@@ -44,7 +44,7 @@ class Dance:
     def _interpolate(self, algorithm: Algorithm) -> List[Dict[str, Any]]:
         interpolated_choreo = []
         for frame, vector_pair in zip(self.choreography, self.vector_pairs()):
-            _, *intermediates, _ = algorithm(*vector_pair, *self.dimensions)
+            _, *intermediates, _ = algorithm(*vector_pair, *self.real_dimensions)
             interpolated_choreo.append(frame)
             for i, intermediate_vector in enumerate(intermediates, 1):
                 interpolated_pos = [
@@ -70,16 +70,17 @@ class Dance:
 def dummy_algo(start: List[Vector], target: List[Vector], x_min: int, y_min: int) -> List[Vector]:
     return [
         start,
-        [s + Vector(1, 0) for s in start],
-        [t + Vector(0, 1) for t in target],
+        [Vector(1, 0) + s for s in start],
+        [Vector(0, 1) + t for t in target],
         target,
     ]
 
 
 if __name__ == "__main__":
     dance = Dance(test_data)
-    for x in dance.vector_pairs():
-        ic(x)
-
     dance.interpolate(dummy_algo)
-    print(json.dumps(dance.to_dict(), indent=4))
+    with open("test_data_dummy_algo.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(dance.to_dict(), indent=4, ensure_ascii=False))
+
+    res = dummy_algo([Vector(1, 2), Vector(3, 4)], [Vector(4, 6), Vector(4, 2)], 10, 10)
+    print(res)
--- server/test_dancers.py
+++ server/test_dancers.py
@@ -1,81 +1,37 @@
 import json
 
 import pytest
-import time
-from server import dance_astar
 
-import server
-from server import dancers
+from server.dancers import dummy_algo, Dance, Vector
 
-TEST_JSON = json.loads(
-"""
-{
-  "title": "Min första lista",
-  "dimensions": {
-    "rows": 12,
-    "columns": 16
-  },
-  "choreography": [
-    {
-      "index": 0,
-      "subindex": 0,
-      "description": "raise your hands",
-      "positions": [
-        {
-          "name": "A",
-          "x": 13,
-          "y": 13,
-          "color": "red"
-        },
-        {
-          "name": "B",
-          "x": 17,
-          "y": 13,
-          "color": "green"
-        },
-        {
-          "name": "C",
-          "x": 21,
-          "y": 13,
-          "color": "blue"
-        }
-      ]
-    },
-    {
-      "index": 1,
-      "subindex": 0,
-      "description": "raise your hands",
-      "positions": [
-        {
-          "name": "A",
-          "x": 13,
-          "y": 11,
-          "color": "red"
-        },
-        {
-          "name": "B",
-          "x": 17,
-          "y": 17,
-          "color": "green"
-        },
-        {
-          "name": "C",
-          "x": 21,
-          "y": 11,
-          "color": "blue"
-        }
-      ]
-    }
-  ],
-  "id": 1
-}
-"""
-)
+with open("test_data.json", encoding="latin-1") as f:
+    test_json = json.load(f)
+
+start = [Vector(13, 13), Vector(17, 13), Vector(21, 13)]
+intermediate_1 = [Vector(14, 13), Vector(18, 13), Vector(22, 13)]
+intermediate_2 = [Vector(13, 12), Vector(17, 18), Vector(21, 12)]
+target = [Vector(13, 11), Vector(17, 17), Vector(21, 11)]
+
+
+def test_dummy_algo():
+    path = [start, intermediate_1, intermediate_2, target]
+    result = dummy_algo(start, target, 0, 0)
+    assert result == path
 
 
 def test_dance_vectors():
-    dance = dancers.Dance(TEST_JSON)
-    assert list(dance._vectors()) == [
-        dancers.Vector(*l) for x in [[(13, 13), (17, 13), (21, 13)], [(13, 11), (17, 17), (21, 11)]] for l in x
+    dance = Dance(test_json)
+    assert dance.id == test_json["id"]
+    assert dance.title == test_json["title"]
+    assert dance.dimensions == test_json["dimensions"]
+
+    assert list(dance._vectors()) == [start, target]
+    assert list(dance.vector_pairs()) == [(start, target)]
+
+    dance.interpolate(dummy_algo)
+
+    assert list(dance.vector_pairs()) == [
+        (start, intermediate_1),
+        (intermediate_1, intermediate_2),
+        (intermediate_2, target),
     ]
-    # assert list(dance.vector_pairs()) == [((13, 13, 17, 13, 21, 13), (13, 11, 17, 17, 21, 11))]
